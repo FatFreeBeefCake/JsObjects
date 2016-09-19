aeddac094ff403a68a9428d0bfac0cd8f1879a6b
Spencer Headman <stheadman1@hotmail.com>
2016-09-19 11:49:29 -0600

    inclass stuff

--- JS/Objects.js
+++ JS/Objects.js
@@ -1,1 +1,26 @@
 // Objects Lesson
+
+var Robot = {
+    fuel: 100,
+    weapon: "laser",
+    speed: 10,
+    strength: 5,
+    armour: "light",
+
+    AddFuel: function (tank){
+        fuel += tank;
+    },
+
+    UpgradeArmour: function (change){
+        amour = change;
+    },
+
+    ChangeWeapon: function (change){
+        weapon = change;
+    }
+}
+
+Robot.speed += parseInt(prompt("Increase Robot speed."));
+alert("Our Robot is now running at " + Robot.speed + " MPH");
+
+Robot.UpgradeArmour(prompt("What should we upgrade the armour to? "));
