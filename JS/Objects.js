4633f515e4881e023c757a02d97fdcfb6da3aa3b
Spencer Headman <stheadman1@hotmail.com>
2016-09-19 11:53:15 -0600

    finished

--- JS/Objects.js
+++ JS/Objects.js
@@ -12,15 +12,16 @@ var Robot = {
     },
 
     UpgradeArmour: function (change){
-        amour = change;
+        this.armour = change;
     },
 
     ChangeWeapon: function (change){
-        weapon = change;
-    }
+        this.weapon = change;
+    },
 }
 
 Robot.speed += parseInt(prompt("Increase Robot speed."));
 alert("Our Robot is now running at " + Robot.speed + " MPH");
 
 Robot.UpgradeArmour(prompt("What should we upgrade the armour to? "));
+alert("Our robot now has " + Robot.armour + " aromur.");
