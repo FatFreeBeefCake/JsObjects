ff6256add0b777103b6155ce8b90c08d6d374740
Spencer Headman <stheadman1@hotmail.com>
2016-09-30 11:47:58 -0600

    9/30/2016 javascript class

--- JS/Event.js
+++ JS/Event.js
@@ -1,15 +1,34 @@
-var button1 = document.getElementById('button1');
 
-
-
-var myFunction = function myFunction() {
-    alert("Hello World");
+// user name button
+var userName;
+    console.log(userName);
+var button1 = document.getElementById('Name');
+var enterName = function enterName() {
+    userName = prompt("Enter Your Name.");
+    alert("Hello " + userName + ".");
+    console.log(userName);
 }
-button1.addEventListener ('click', myFunction);
+button1.addEventListener ('click', enterName);
 
+// start button
 var startButton = document.getElementById('startGame');
-
 var startGameHandler = function startMyGame (){
-    alert("youve started the game");
+    if(userName === undefined){
+        alert("Please enter a valid name.");
+    } else if (userName != undefined){
+        alert("Welcome " + userName + " the game has begun!");
+    }
 }
 startButton.addEventListener('click',startGameHandler);
+
+var gameButton = function gameButtonConstruct (name){
+    var button;
+    var fire = function (){
+        alert("fire");
+    }
+}
+
+var fireweaponButton = new gameButton();
+fireweaponButton.button = document.getElementById('fireWeapon');
+fireweaponButton.button.addEventListener('click', fireweaponButton.fire);
+
